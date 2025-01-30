d14e916eb0d05f730852d7b74003771177379ecb
KristijanPoposki <kristijanpoposki@yahoo.com>
2025-01-30 03:44:55 +0100

    Initial commit

---
+++ DXWebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DXWebApplication1.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Designer() {
+            Models.ReportDesignerModel model = new Models.ReportDesignerModel();
+            return View(model);
+        }
+
+        public ActionResult Viewer() {
+            return View();
+        }
+    }
+}
